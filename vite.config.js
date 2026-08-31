a7b3573b622ef2f29ff52ca38c57228390d3d143
AlgazinProject <algazunv@bk.ru_GITHUB@example.com>
2026-08-31 17:42:06 +0600

    fix: set empty base for relative paths

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev
 export default defineConfig({
   plugins: [react()],
+  base: '', // ИСПРАВЛЕНО: Этот параметр делает все пути относительными, убирая белый экран на любом хостинге!
 })
